7bd32fbb1fcbde6382ba6fdd27ac9bd7f04985f5
ravraw <ravinrawat.wd@gmail.com>
2025-09-02 18:37:00 -0600

    HuggingFaceEndpoint chatModel

---
+++ LLM/chat_hf_api.py
@@ -0,0 +1,18 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1-0528",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
+    provider="auto",  # let Hugging Face choose the best provider for you
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+
+response = chat_model.invoke("whats the capital of USA")
+
+print(response)
