0d6db2ccea3332f2aeb005b40cc1308977c65182
ravraw <ravinrawat.wd@gmail.com>
2025-09-11 19:50:37 -0600

    Using PDFPLumber - not working

---
+++ Document_loaders/pdf_loader.py
@@ -0,0 +1,10 @@
+# from langchain_community.document_loaders import PDFPlumberLoader
+
+# loader = PDFPlumberLoader("flyer.pdf")
+
+# docs = loader.load()
+
+# print(len(docs))
+
+# print(docs[1].page_content)
+# print(docs[1].metadata)
